cfd7dc684611457e7310fe51d1a97a29ace38af9
Nick Nissen <nickknissen@gmail.com>
2015-11-14 12:18:07 +0100

    Feat: Setup simple page with latest bootstrap and angular

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<title>Royalbeer Music Player</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<link rel=stylesheet" href="/css/main.css">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
+	<script src="/js/app.js"></script>
+</head>
+<body>
+	<header></header>
+	<main>
+		<h1>Royalbeer Music Player</h1>
+		<div>
+		</div>
+	</main>
+	<footer></footer>
+</body>
+</html>
+
